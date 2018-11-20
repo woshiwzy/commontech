ba9390974b24835eec8dc10c93cda8c0d17573ab
王正云 <wangzhengyun@travelsky.com>
2018-11-20 13:07:11 +0800

    first commit

---
+++ common_net/src/main/java/com/commontech/common_net/HttpsTool.java
@@ -0,0 +1,47 @@
+package com.commontech.common_net;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+/**
+ * Created by wangzy on 2018/11/20
+ * description:
+ */
+public class HttpsTool {
+
+
+
+
+    /**
+     * 访问网络之前call 这个方法信任Https 服务器
+     */
+    public static void setUpSSL() {
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+
+                    public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                        //No need to implement.
+                    }
+
+                    public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                        //No need to implement.
+                    }
+                }
+        };
+
+        // Install the all-trusting trust manager
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
